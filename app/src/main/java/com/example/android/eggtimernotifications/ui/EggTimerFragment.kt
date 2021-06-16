6e273da82969802e2b90ac45ec469116659980bd
rodriguesporan <rodriguesporan@gmail.com>
2021-06-16 08:28:14 -0300

    feat: added Firebase Cloud Messaging

--- app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/ui/EggTimerFragment.kt
@@ -24,11 +24,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.android.eggtimernotifications.R
 import com.example.android.eggtimernotifications.databinding.FragmentEggTimerBinding
+import com.google.firebase.messaging.FirebaseMessaging
 
 class EggTimerFragment : Fragment() {
 
@@ -53,6 +55,13 @@ class EggTimerFragment : Fragment() {
                 getString(R.string.egg_notification_channel_name)
         )
 
+        createChannel(
+                getString(R.string.breakfast_notification_channel_id),
+                getString(R.string.breakfast_notification_channel_name)
+        )
+
+        subscribeTopic()
+
         return binding.root
     }
 
@@ -73,6 +82,17 @@ class EggTimerFragment : Fragment() {
             val notificationManager = requireContext().getSystemService(NotificationManager::class.java)
             notificationManager.createNotificationChannel(notificationChannel)
         }
+    }
+
+    private fun subscribeTopic() {
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+                .addOnCompleteListener { task ->
+                    var message = getString(R.string.message_subscribed)
+                    if (!task.isSuccessful) {
+                        message = getString(R.string.message_subscribe_failed)
+                    }
+                    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+                }
     }
 
     companion object {
